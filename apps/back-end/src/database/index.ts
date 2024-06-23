32fc8f03c1fdd3a6d35b19f5b33c3387be19b2c0
ivymuchacha <ivy83718@gmail.com>
2024-06-23 18:31:39 +0800

    init: database

---
+++ apps/back-end/src/database/index.ts
@@ -0,0 +1,32 @@
+import { Sequelize } from 'sequelize';
+
+// 從環境變量中獲取資料庫配置
+const host: string = process.env.POSTGRES_HOST!;
+const port: number = parseInt(process.env.POSTGRES_PORT!, 10);
+const database: string = process.env.POSTGRES_DB!;
+const username: string = process.env.POSTGRES_USER!;
+const password: string = process.env.POSTGRES_PASSWORD!;
+
+// 創建 Sequelize 實例
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  port: port,
+  dialect: 'postgres',
+  logging: console.log, // 可以設置為 true 來啟用 SQL 日誌
+});
+
+// 測試連接
+async function authenticateDatabase() {
+  try {
+    await sequelize.authenticate();
+    console.log(host, port, database, username, password);
+    console.log('Connection has been established successfully.');
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+  }
+}
+
+// 調用驗證函數
+authenticateDatabase();
+
+export default sequelize;

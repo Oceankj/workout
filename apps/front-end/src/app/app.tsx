86db0d0210477da92f62d8a95a9384783a1269f3
Kao <0980124jim@gmail.com>
2024-06-27 18:07:27 +0800

    feat(component): add button

--- apps/front-end/src/app/app.tsx
+++ apps/front-end/src/app/app.tsx
@@ -1,10 +1,18 @@
-import { Outlet } from 'react-router-dom';
-// import NxWelcome from './nx-welcome';
+import { Link, Outlet } from 'react-router-dom';
+import { Button } from './components/button';
+// import { Button } from '@headlessui/react';
 
 export function App() {
   return (
     <div>
-      {/* <NxWelcome title="front-end" /> */}
+      <nav className="border-b border-solid border-gray-300 flex items-center justify-center py-2 space-x-2">
+        <Link className="outline-none" to={''}>
+          <Button>Home Page</Button>
+        </Link>
+        <Link className="outline-none" to={`login`}>
+          <Button layout="secondary">Login Page</Button>
+        </Link>
+      </nav>
       <Outlet />
     </div>
   );
